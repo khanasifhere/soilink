b5bb231d4039d6089b8f89df18e0e37cefb142d8
asif <khanaasif1065@gmail.com>
2025-08-01 00:40:35 +0530

    fix some bugs again

--- client/src/pages/OrderFormPage.jsx
+++ client/src/pages/OrderFormPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { useDispatch,useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { placeOrder } from '../store/slices/orderSlice';
--- client/src/pages/RentalFormPage.jsx
+++ client/src/pages/RentalFormPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { useDispatch,useSelector } from 'react-redux';
 import { createRentalRequest } from '../store/slices/rentalSlice';
 import { useParams, useNavigate, Link } from 'react-router-dom';
